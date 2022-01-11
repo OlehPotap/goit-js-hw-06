b031518bdbd34d15dd14c788916c048d43ed0031
OlehPotap <potap.oleh.v@gmail.com>
2022-01-11 16:54:48 +0200

    task06

---
+++ src/js/task-06.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const inputEl = document.querySelector("#validation-input");
+inputEl.addEventListener("blur", (event) => {
+  if (event.currentTarget.value.length !== Number(inputEl.dataset.length)) {
+    inputEl.classList.add("invalid");
+  } else {
+    inputEl.classList.remove("invalid");
+    inputEl.classList.add("valid");
+  }
+});
