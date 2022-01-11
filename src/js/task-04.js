cc1f0ef5e8d30e37459942bc190780571df29145
OlehPotap <potap.oleh.v@gmail.com>
2022-01-11 15:20:28 +0200

    task4

---
+++ src/js/task-04.js
@@ -0,0 +1,20 @@
+"use strict";
+let counterValue = 0;
+
+const counterValueDecrement = document.querySelector(
+  '[data-action="decrement"]'
+);
+const counterValueIncrement = document.querySelector(
+  '[data-action="increment"]'
+);
+
+const counterValueEl = document.querySelector("#value");
+
+counterValueDecrement.addEventListener("click", () => {
+  counterValue -= 1;
+  counterValueEl.innerHTML = counterValue;
+});
+counterValueIncrement.addEventListener("click", () => {
+  counterValue += 1;
+  counterValueEl.innerHTML = counterValue;
+});
