d578b83258dcdb930dadbe4685152e3e3237b463
OlehPotap <potap.oleh.v@gmail.com>
2022-01-12 12:33:35 +0200

    task09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,19 @@
+"use strict";
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const bgClrInformerEl = document.querySelector(".color");
+const bgClrChangeBtnEl = document.querySelector(".change-color");
+
+bgClrChangeBtnEl.addEventListener("click", (event) => {
+  bodyEl.style.background = getRandomHexColor();
+
+  // Вывод занчения цвета в RGB
+  // bgClrInformerEl.textContent = bodyEl.style.background;
+
+  // Вывод значения цвета в HEX
+  bgClrInformerEl.textContent = getRandomHexColor();
+});
